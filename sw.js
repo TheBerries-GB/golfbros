bf8b3046eef21837872ea09b8782fef13c816421
TheBerries-GB <henry.g.young81@gmail.com>
2026-06-09 09:55:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'golfbros-v1';
+var CACHE = 'golfbros-v3';
 var ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', function(e) {
